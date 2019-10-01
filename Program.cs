2cb57fcbc27f4242ed56e905dae3c99cc8d0e055
hoanglong151 <55777965+hoanglong151@users.noreply.github.com>
2019-10-01 10:33:15 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello github");
+            Console.WriteLine(pow(2,5));
+            Console.WriteLine(abs(-5));
+           
+        }
+        public static bool IsOdd(int n)
+        {
+            for (int i = 0; i < n; i++ )
+            {
+                if (n % 2 == 0)
+                    return false;
+            }
+            return true;
+        }
+        public static bool IsEven(int n)
+        {
+            for (int i = 0; i < n;i++ )
+            {
+                if (n % 2 == 0)
+                    return true;
+            }
+            return false;
+
+        }
+        public static bool IsPrime(int n)
+        {
+            if(n<=1)
+            {
+                return false;
+            }
+            for(int i = 2 ; i < n ; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        public static int pow (int x , int y)
+        {
+            int r = 1;
+            for(int i = 1 ; i <= y ; i++)
+            {
+                r = r * x;
+            }
+            return r;
+        }
+        public static int abs(int n)
+        {
+          if ( n < 0)
+              n = n * -1;
+          return n;
+        }
+        }
+    }
+
+
+
---
+++ UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool res03 = Program.IsOdd(3);
+            Assert.AreEqual(true, res03);
+            bool res04 = Program.IsOdd(4);
+            Assert.AreEqual(false, res04);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            bool res03 = Program.IsEven(3);
+            Assert.AreEqual(false, res03);
+            bool res04 = Program.IsEven(4);
+            Assert.AreEqual(true, res04);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            bool res03 = Program.IsPrime(3);
+            Assert.AreEqual(true, res03);
+            bool res04 = Program.IsPrime(4);
+            Assert.AreEqual(false, res04);
+        }
+
+    }
+}
